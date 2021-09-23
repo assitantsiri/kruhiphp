a9548ff745fa1297f71a86b5161ef1ad9e4384fa
Ankesh Kumar <ankeshrajput9f@gmail.com>
2021-09-23 22:10:41 +0530

    initial commit

---
+++ registationphp.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Name is :-".$_POST['name']."<br/>";
+echo "Mobile Number is :- ".$_POST['mobileno']."<br/>";
+echo "Email Address is :-".$_POST['email']."<br/>";
+echo "Password is :-".$_POST['passwd']."<br/>";
+
+?>
